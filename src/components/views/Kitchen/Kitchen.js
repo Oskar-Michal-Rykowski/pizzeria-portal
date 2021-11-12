d64e6dec286110877273c62aa16058ac2600b8a7
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-11-12 12:02:26 +0100

    Remove comments

--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -11,116 +10,51 @@ import Switch from '@material-ui/core/Switch';
 
 import styles from './Kitchen.module.scss';
 
-const greenSwitch = withStyles()(Switch);
-
-// const themeOrders = [
-//   {
-//     type: 'local',
-//     done: true,
-//     number: '1',
-//     order: [
-//       {
-//         name: `Zia Giulia's Breakfast`,
-//         params: [{ name: 'Coffee type', choosen: ['Latte'] }],
-//       },
-//       {
-//         name: `Nonna Alba's Pizza`,
-//         params: [
-//           { name: 'Sauce', choosen: ['Tomato'] },
-//           {
-//             name: 'Toppings',
-//             choosen: ['Olives', 'Red peppers', 'Green peppers'],
-//           },
-//           { name: 'Pizza crust', choosen: ['standard'] },
-//         ],
-//       },
-//     ],
-//   },
-//   {
-//     type: 'remote',
-//     number: '123',
-//     done: false,
-//     order: [
-//       {
-//         name: `Zia Giulia's Breakfast`,
-//         params: [{ name: 'Coffee type', choosen: ['Cappuccino'] }],
-//       },
-//       {
-//         name: `Nonno Alberto's Salad`,
-//         params: [{ name: 'Ingredients', choosen: ['Cucumber', 'Tomatoes'] }],
-//       },
-//     ],
-//   },
-// ];
+const themeOrders = [
+  {
+    type: 'local',
+    done: true,
+    number: '1',
+    order: [
+      {
+        name: `Zia Giulia's Breakfast`,
+        params: [{ name: 'Coffee type', choosen: ['Latte'] }],
+      },
+      {
+        name: `Nonna Alba's Pizza`,
+        params: [
+          { name: 'Sauce', choosen: ['Tomato'] },
+          {
+            name: 'Toppings',
+            choosen: ['Olives', 'Red peppers', 'Green peppers'],
+          },
+          { name: 'Pizza crust', choosen: ['standard'] },
+        ],
+      },
+    ],
+  },
+  {
+    type: 'remote',
+    number: '123',
+    done: false,
+    order: [
+      {
+        name: `Zia Giulia's Breakfast`,
+        params: [{ name: 'Coffee type', choosen: ['Cappuccino'] }],
+      },
+      {
+        name: `Nonno Alberto's Salad`,
+        params: [{ name: 'Ingredients', choosen: ['Cucumber', 'Tomatoes'] }],
+      },
+    ],
+  },
+];
 
 export default function Kitchen() {
-  // const switchStyle = styles.switchChecked;
-  // let switchChecked = true;
-
-  // function handleSwitch(boolean) {
-  //   switchChecked = !boolean;
-  //   return switchChecked;
-  // }
-
-  const [state, setState] = React.useState([
-    {
-      type: 'local',
-      done: true,
-      number: '1',
-      order: [
-        {
-          name: `Zia Giulia's Breakfast`,
-          params: [{ name: 'Coffee type', choosen: ['Latte'] }],
-        },
-        {
-          name: `Nonna Alba's Pizza`,
-          params: [
-            { name: 'Sauce', choosen: ['Tomato'] },
-            {
-              name: 'Toppings',
-              choosen: ['Olives', 'Red peppers', 'Green peppers'],
-            },
-            { name: 'Pizza crust', choosen: ['standard'] },
-          ],
-        },
-      ],
-    },
-    {
-      type: 'remote',
-      number: '123',
-      done: false,
-      order: [
-        {
-          name: `Zia Giulia's Breakfast`,
-          params: [{ name: 'Coffee type', choosen: ['Cappuccino'] }],
-        },
-        {
-          name: `Nonno Alberto's Salad`,
-          params: [{ name: 'Ingredients', choosen: ['Cucumber', 'Tomatoes'] }],
-        },
-      ],
-    },
-  ]);
-
-  // const handleChange = (event) => {
-  //   for (let order of state) {
-  //     if (order.number === event.target.number) {
-  //       setState([
-  //         ...state,
-  //         {
-  //           number: event.target.number,
-  //           done: event.target.checked,
-  //           ...order,
-  //         },
-  //       ]);
-  //     }
-  //   }
-  // };
-
   return (
     <div className={styles.component}>
       <h2>Kitchen</h2>
-      {state.map((order) => (
+      {themeOrders.map((order) => (
         <Card
           key={order.number}
           className={`${styles.root} ${
